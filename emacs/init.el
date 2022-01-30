a1a3eaaf5d142baa51046168f0f79c8439a62960
AndreiD049 <andrei.dimitrascu.94@gmail.com>
2022-01-30 11:42:47 +0200

    added ivy completion

--- emacs/init.el
+++ emacs/init.el
@@ -24,6 +24,25 @@
 (setq use-package-always-ensure t)
 ;; ======================================
 
+;; Completion
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
 ;; Basic conifgurations
 (setq inhibit-startup-message t)
 
