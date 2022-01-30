5f6ded1d6621b2a1de0fc3d9d885e9fbbc9ef036
AndreiD049 <andrei.dimitrascu.94@gmail.com>
2022-01-30 11:20:06 +0200

    added use-package

--- emacs/init.el
+++ emacs/init.el
@@ -1,11 +1,42 @@
-;; Basic conifgurations
-(setq scroll-bar-mode nil)
-
+;; Setting custom configuration file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(message custom-file)
+(when (file-exists-p custom-file)
+  (load-file custom-file))
 
+;; ======================================
+;; Setting package manager & adding melpa
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Init use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+;; use-package installed
+(require 'use-package)
+(setq use-package-always-ensure t)
+;; ======================================
+
+;; Basic conifgurations
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode nil)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(set-fringe-mode 10)
+
+;; Disable bell
+(setq visible-bell t)
+
+;; Font configuration
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font Mono" :height 110)
 
 ;; Load color theme
 (load-theme 'dracula t)
